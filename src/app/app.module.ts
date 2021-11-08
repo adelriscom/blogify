de8eebb3e19d38382ab3b3a717b6f7e0c9ac5ad6
alexander.delrisco@outlook.com <alexander.delrisco@outlook.com>
2021-11-08 18:07:05 -0500

    Adding material and some events

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
+// Librerias de angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
 
+// Componentes del aplicativo
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+
+// Componentes de Material
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatePostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, MatButtonModule, MatInputModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/create-post/create-post.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-post',
+  templateUrl: './create-post.component.html',
+  styleUrls: ['./create-post.component.css']
+})
+export class CreatePostComponent implements OnInit {
+
+  content: string = 'Hola Mundo';
+  texto: string = '';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  showText(){
+    this.content = this.texto;
+  }
+
+}
